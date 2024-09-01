bfc04188eadae4ac908e1c7ef49d7fcd08ce092f
lehuy <lehuy.nguyen@ekino.com>
2024-09-01 11:35:26 +0700

    Eps 11

---
+++ tests/Unit/Talks/SaveTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Tests\Unit\Talks;
+
+use Tests\Support\UnitTester;
+
+class SaveTest extends \Codeception\Test\Unit
+{
+
+    protected UnitTester $tester;
+
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
